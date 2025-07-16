b4b7519ee307e55cd9005b7843b70f0e4dfa9f7f
Nikita Ryzhenko <ryzhenkonikita6@gmail.com>
2025-07-16 11:46:47 +0300

    Added exception for not valid json input. Added Annotation

--- api/routers/items.py
+++ api/routers/items.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Query, Path, Body
 from ...db.database import client, db, collection
 from contextlib import asynccontextmanager
 from logging import info 
 import logging
 import json
-from typing import Dict, List
+from typing import Annotated, Dict, List
 
 router = APIRouter()
 logging.basicConfig(level=logging.INFO)
@@ -27,17 +27,45 @@ async def db_lifespan(app: APIRouter):
 router: APIRouter = APIRouter(lifespan=db_lifespan)
 
 @router.get("/find/one/{query_str}", status_code=status.HTTP_200_OK)
-async def get_page(query_str:str):
-    query: Dict = json.loads(query_str)
+async def get_page(query_str:Annotated[str, Path(max_length=3000,
+                                                  min_length=7,
+                                                  title="Query JSON",
+                                                  description="""Query parameter for searching
+                                                  in MongoDB DataBase""")]):
+    
+    try:
+        query: Dict = json.loads(query_str)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400,
+                            detail="Invalid data input",
+                            headers={"X-Error": "InvalidJSON"})
+        
     document = await collection.find_one(query)
+    
     if document is None:
-        raise HTTPException(status_code=204, detail="Query doesn't match any data in the db")
+        raise HTTPException(status_code=204,
+                            detail="data not found",
+                            headers={"X-Error": "Query doesn't match any data in the db"})
     document["_id"] = str(document["_id"])
+    
     return document
     
 @router.get("/find/all/", status_code=status.HTTP_200_OK)
-async def get_pages(query_str:str = "{}"):
-    query: Dict = json.loads(query_str)
+async def get_pages(query_str:Annotated[str, Path(max_length=3000,
+                                                   min_length=7,
+                                                  title="Query JSON",
+                                                  description="""Query parameter for searching
+                                                  in MongoDB DataBase. By
+                                                   putting the default value, get  all
+                                                   documents in the collection""")] = "{}"):
+    
+    try:
+        query: Dict = json.loads(query_str)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400,
+                            detail="Invalid data input",
+                            headers={"X-Error": "InvalidJSON"})
+        
     cursor = collection.find(query)
     documents = []
     async for document in cursor:  # Use async for here
@@ -49,33 +77,80 @@ async def get_pages(query_str:str = "{}"):
     else: return documents
 
 @router.post("/content/{content_str}", status_code=status.HTTP_201_CREATED)
-async def post_page(content_str: str):
-    content: Dict = json.loads(content_str)
+async def post_page(content_str: Annotated[str, Path(max_length=30000,
+                                                      min_length=7,
+                                                  title="Document JSON",
+                                                  description="""Document to insert 
+                                                  the MongoDB DataBase""")]):
+    
+    try:
+        content: Dict = json.loads(content_str)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400,
+                            detail="Invalid data input",
+                            headers={"X-Error": "InvalidJSON"})
+        
     # Insert the structured result into MongoDB
     await collection.insert_one(content)
     return {"message": "Content and inserted"}
 
 @router.post("/contents/{contents_str}", status_code=status.HTTP_201_CREATED)
-async def post_pages(contents_str, skip_error:bool):    
+async def post_pages(contents_str:Annotated[List[str], Body(title="Documents JSON",
+                                                             description="""List of documents
+                                                             to inseart to 
+                                                             the MongoDB DataBase""")], 
+                     skip_error:Annotated[bool, Query(description="""Define a flag to skip
+                                                      collections with error message
+                                                      or not. By default, not skipping""")] = False):    
+    
     contents_dict: List[Dict] = []
     for index, content in enumerate(contents_str):
         if ("error" in content) and (skip_error == True): 
             continue
         else:
-            data: Dict = json.loads(content)
+            try:
+                data: Dict = json.loads(content)
+            except json.JSONDecodeError:
+                raise HTTPException(status_code=400,
+                                    detail="Invalid data input",
+                                    headers={"X-Error": f"InvalidJSON: {content}"})
             contents_dict.append(data) 
     await collection.insert_many(contents_dict)
         
     return {"message": "Page scraped and inserted"}
 
 @router.delete("/delete/single/{query_str}")
-async def get_pages(query_str:str):
-    query: Dict = json.loads(query_str)
+async def get_pages(query_str:Annotated[str, Path(max_length=30000,
+                                                      min_length=7,
+                                                  title="Query JSON",
+                                                  description="""Delete the first match 
+                                                  with the query""")]):
+    
+    try:
+        query: Dict = json.loads(query_str)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400,
+                            detail="Invalid data input",
+                            headers={"X-Error": "InvalidJSON"})
+        
     await collection.delete_one(query)
+    
     return {"message": f"Query deleted successful: {query}"}
 
 @router.delete("/delete/all/{query_str}")
-async def get_pages(query_str:str):
-    query: Dict = json.loads(query_str)
+async def get_pages(query_str:Annotated[str, Path(max_length=30000,
+                                                      min_length=7,
+                                                  title="Query JSON",
+                                                  description="""Query that matches the document
+                                                  to delete""")]):
+    
+    try:
+        query: Dict = json.loads(query_str)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400,
+                            detail="Invalid data input",
+                            headers={"X-Error": "InvalidJSON"})
+        
     await collection.delete_many(query)
+    
     return {"message": f"Querys deleted successful: {query}"}
--- parsing/scrape.py
+++ parsing/scrape.py
@@ -6,24 +6,6 @@ import dotenv
 import requests
 import types
 
-urls = ["https://futurumcareers.com/articles",
-        "https://www.science.org/content/page/subject-collections",  # Genetics & Molecular Biology                      # Neuroscience
-    "https://www.science.org/journal/sciimmunol",               # Immunology
-
-    # Physical Sciences
-    "https://www.sciencedaily.com/news/matter_energy/physics/", # Physics
-    "https://phys.org/chemistry-news/",                         # Chemistry
-    "https://science.nasa.gov",                                 # Astronomy & Space Science
-    "https://www.issibern.ch/scientific-opportunities/forums/", # Space Science Forums
-
-    # Environmental & Earth Sciences
-    "https://www.sciencedaily.com/news/earth_climate/climate/", # Climate & Environment
-    "https://www.bgs.ac.uk/news/",                              # Geosciences
-
-    # Multidisciplinary & Forums
-    "https://www.scienceforums.net/",                           # General Science Forums
-    "https://ncas.ac.uk/for-staff/research-forums/"]
-
 dotenv.load_dotenv()
 
 class Firecrawl:
@@ -87,6 +69,25 @@ class Firecrawl:
                 return error_model.model_dump_json()
 
 if __name__ == "__main__":
+    
+    urls = ["https://futurumcareers.com/articles",
+        "https://www.science.org/content/page/subject-collections",  # Genetics & Molecular Biology                      # Neuroscience
+    "https://www.science.org/journal/sciimmunol",               # Immunology
+
+    # Physical Sciences
+    "https://www.sciencedaily.com/news/matter_energy/physics/", # Physics
+    "https://phys.org/chemistry-news/",                         # Chemistry
+    "https://science.nasa.gov",                                 # Astronomy & Space Science
+    "https://www.issibern.ch/scientific-opportunities/forums/", # Space Science Forums
+
+    # Environmental & Earth Sciences
+    "https://www.sciencedaily.com/news/earth_climate/climate/", # Climate & Environment
+    "https://www.bgs.ac.uk/news/",                              # Geosciences
+
+    # Multidisciplinary & Forums
+    "https://www.scienceforums.net/",                           # General Science Forums
+    "https://ncas.ac.uk/for-staff/research-forums/"]
+    
     app = Firecrawl()
     for url in urls:
         result = app.get_structured_output(url=url)
